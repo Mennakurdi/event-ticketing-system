6929449c6669409d76234ee32c8497ccf6f21e82
malakalakkad <malakalakkad445@gmail.com>
2025-05-27 11:43:25 +0300

    update admin usermodel

--- frontend/my-react-app/src/components/UpdateUserRoleModal.jsx
+++ frontend/my-react-app/src/components/UpdateUserRoleModal.jsx
@@ -18,7 +18,7 @@ export default function UpdateUserRoleModal({ user, onClose, onRoleUpdated }) {
       setLoading(false);
     }
   };
-
+vi
   return (
     <div className="fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center z-50">
       <div className="bg-white p-6 rounded w-[300px]">
